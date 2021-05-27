99147fa6a7e0a46bca592fc15c8cf1f4861a3d9d
datta1808 <bagaledatta18@gmail.com>
2021-05-27 23:02:57 +0530

    ADDED: Functionality to get Total Wage queried by company

--- src/main/java/com/bridgelabz/EmployeeWageMain.java
+++ src/main/java/com/bridgelabz/EmployeeWageMain.java
@@ -1,10 +1,14 @@
 package com.bridgelabz;
 
+
 public class EmployeeWageMain {
     public static void main(String[] args) {
         EmployeeWageBuilder employeeWageBuilder = new EmployeeWageBuilder();
         employeeWageBuilder.addCompanyEmpWage("Amazon", 18, 18, 90);
         employeeWageBuilder.addCompanyEmpWage("Flipkart", 25, 24, 100);
         employeeWageBuilder.computeEmpWage();
+
+        // getting total wage when queried by the company
+        System.out.println("Total Wage for Amazon : " + employeeWageBuilder.getTotalWage("Amazon"));
     }
 }
