46f511028366aa2899c34c7920b73da2326a1dab
datta1808 <bagaledatta18@gmail.com>
2021-05-27 18:48:01 +0530

    ADDED: Functionality to compute Employee Wage for Multiple Companies

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,20 +1,19 @@
 package com.bridgelabz;
 
 public class EmployeeWageBuilder {
-    final int IS_PART_TIME = 1;
-    final int IS_FULL_TIME = 2;
-    final int WAGE_PER_HOUR = 20;
-    final int NO_OF_WORKING_DAYS = 20;
-    final int MAX_WORKING_HOURS = 100;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
 
-    public void calculateTotalWage() {
-        int empHours, totalEmpHours = 0, totalWorkingDays = 0;
+    public static int calculateTotalWage(String company, int empRate, int noOfDays, int maxHrs) {
+        //variables
+        int empHours=0, totalEmpHours = 0, totalWorkingDays = 0;
 
-        while (totalEmpHours <= MAX_WORKING_HOURS && totalWorkingDays < NO_OF_WORKING_DAYS){
+        //Computation
+        while (totalEmpHours <= maxHrs && totalWorkingDays < noOfDays) {
             totalWorkingDays++;
             int empCheck = (int) ((Math.random() * 10) % 3);
 
-            switch(empCheck) {
+            switch (empCheck) {
                 case IS_FULL_TIME:
                     System.out.println("Full Time Employee");
                     empHours = 8;
@@ -28,15 +27,16 @@ public class EmployeeWageBuilder {
                     empHours = 0;
             }
             totalEmpHours += empHours;
-            System.out.println("Total working hours: " +totalEmpHours);
-            System.out.println("Total working days: " +totalWorkingDays);
+            System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHours);
         }
-        int totalEmpWage = totalEmpHours * WAGE_PER_HOUR;
-        System.out.println("Total wage of Employee is: " + totalEmpWage);
+        int totalEmpWage = totalEmpHours * empRate;
+        System.out.println("Total wage for Company : "+company+" is: " + totalEmpWage);
+        return totalEmpWage;
     }
 
+
     public static void main(String[] args) {
-        EmployeeWageBuilder employee = new EmployeeWageBuilder();
-        employee.calculateTotalWage();
+        calculateTotalWage("Amazon",20, 2, 10);
+        calculateTotalWage("flipkart",10, 4, 20);
     }
 }
