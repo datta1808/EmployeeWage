fd26d3aa729257e8c168f494e37c0b642cee90db
datta1808 <bagaledatta18@gmail.com>
2021-05-27 18:27:17 +0530

    REFACTORED: Modified code using Switch Case

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -6,16 +6,21 @@ public class EmployeeWageBuilder {
         final int IS_FULL_TIME = 2;
         final int WAGE_PER_HOUR = 20;
         int empHours = 0;
-        double empCheck = Math.floor((Math.random() * 10) % 3);
-        if(empCheck == IS_FULL_TIME) {
-            System.out.println("Full Time Employee");
-            empHours = 8;
-        } else if(empCheck == IS_PART_TIME){
-            System.out.println("Part Time Employee");
-            empHours = 4;
-        } else {
-            System.out.println("Employee is absent");
+        int empCheck = (int)((Math.random() * 10) % 3);
+
+        switch(empCheck){
+            case IS_FULL_TIME:
+                System.out.println("Full Time Employee");
+                empHours = 8;
+                break;
+            case IS_PART_TIME:
+                System.out.println("Part Time Employee");
+                empHours = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
+
         System.out.println("Daily Work Hours: " + empHours);
         int salary = empHours * WAGE_PER_HOUR;
         System.out.println("Daily wage of Employee is: " + salary);
