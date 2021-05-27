7b29e75fed2ee1a23e7bffe8554a843bb9c379af
datta1808 <bagaledatta18@gmail.com>
2021-05-27 18:30:41 +0530

    ADDED: Functionality to calculate Monthly Wage

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -5,24 +5,32 @@ public class EmployeeWageBuilder {
         final int IS_PART_TIME = 1;
         final int IS_FULL_TIME = 2;
         final int WAGE_PER_HOUR = 20;
+        final int NO_OF_WORKING_DAYS = 20;
+
         int empHours = 0;
-        int empCheck = (int)((Math.random() * 10) % 3);
+        int totalSalary = 20;
 
-        switch(empCheck){
-            case IS_FULL_TIME:
-                System.out.println("Full Time Employee");
-                empHours = 8;
-                break;
-            case IS_PART_TIME:
-                System.out.println("Part Time Employee");
-                empHours = 4;
-                break;
-            default:
-                System.out.println("Employee is absent");
+        for (int day = 1; day <= NO_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) ((Math.random() * 10) % 3);
+
+            switch(empCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Full Time Employee");
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Part Time Employee");
+                    empHours = 4;
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+                    empHours = 0;
+            }
+
+            int salary = empHours * WAGE_PER_HOUR;
+            System.out.println(salary);
+            totalSalary += salary;
         }
-
-        System.out.println("Daily Work Hours: " + empHours);
-        int salary = empHours * WAGE_PER_HOUR;
-        System.out.println("Daily wage of Employee is: " + salary);
+        System.out.println("Total wage of Employee is: " + totalSalary);
     }
 }
